38eca959c65af63a5b646c214fea7ac16eef9d5b
root <root@ansible.example.com>
2022-05-20 17:58:57 +0530

    test1

---
+++ main.tf
@@ -0,0 +1,12 @@
+rource "aws_dynamodb_table" "mytest-table" {
+  name           = "GameScores"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "UserId"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+}
